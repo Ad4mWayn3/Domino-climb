1b5d640f149cd311d0eda09b3de49d9c006082e9
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-01-11 20:16:32 -0500

    added player constructor without texture parameter

--- include/platformer.hpp
+++ include/platformer.hpp
@@ -2,7 +2,7 @@
 
 #include <raylib.h>
 #include <raymath.h>
-
+ 
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -15,14 +15,19 @@ const struct {
 	int width, height;
 } resolution{1920, 1080};
 
+bool checkCollisions(Rectangle rec, Rectangles recs);
+
+enum Axis {horizontal, vertical};
+
+bool checkAdjacents(Rectangle rec, Rectangles recs, Axis axis, bool& onGround);
+
 class Player {
 	int x, y;
 	int height, width;
-	bool collidingX=false, collidingY=false;
 
 	void moveAxis(int moveAm, bool& axisCollide, Vector2 shift, Rectangles recs) {
 		while (moveAm) {
-			if (checkCollisions({float(x+shift.x), float(y+shift.y), \
+			if (!checkCollisions({float(x+shift.x), float(y+shift.y), \
 				(float)width, (float)height}, recs)) {
 				axisCollide = true;
 				return;
@@ -35,14 +40,19 @@ class Player {
 	}
 public:
 	Texture tex;
+	bool collidingX=false, collidingY=false, onGround=false;
+	Player(int _x, int _y, int _width, int _height)
+	:	x{_x}, y{_y}
+	,	width{_width}, height{_height}{}
+
 	Player(int _x, int _y, int _width, int _height, Texture _tex)
 	:	x{_x}, y{_y}
 	,	width{_width}, height{_height}
 	,	tex{_tex}{}
 
 	Vector2 position(){ return {(float)x, (float)y}; }
-	Rectangle rectangle(){ return {(float)x, (float)y, (float)width,
-		(float)height};
+	Rectangle rectangle(){
+		return {(float)x, (float)y, (float)width, (float)height};
 	}
 
 	Player& moveX(float speed, Rectangles recs);
@@ -64,10 +74,11 @@ struct GameData{
 	Vector2 maxVel{454,1200};
 	Vector2 accel{1777,0};
 	Vector2 gravity{0,1654};
+	Player _player{resolution.width/2.0f, resolution.height/2.0f, 60, 60};
 	float jumpImpulse = 647;
 };
 
 
 void _process(float delta, GameData& gameData);
 
-bool checkCollisions(Rectangle rec, Rectangles recs);
+void process(float delta, GameData& gameData);
