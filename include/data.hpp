487ef3b1864ea838cba19d445ee2b89b7571247c
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-02-18 23:01:49 -0500

    global level datatypes, variables and constants now isolated here

---
+++ include/data.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <raylib.h>
+#include <raymath.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+
+#define MIN_BOUNCE_SPEED 100
+
+using Rectangles = const std::vector<Rectangle>&;
+
+const struct {
+	int width, height;
+} resolution{1920, 1080};
+
+const Vector2 resolutionV{(float)resolution.width, (float)resolution.height};
+
+enum CamMode{mouselook=0, follow};
+
+enum Axis {horizontal, vertical};
+
+enum EditControls {
+	scroll=0, select, create,
+	resize_w, resize_h, move_up, move_down, move_left, move_right,
+	copy, _delete,
+	save, load
+};
+
+const int editControlsCount = 13;
+
+enum Controls{up=0, down, left, right, jump, look, toggleCamera, reset};
+
+struct Control { int key; const char* name; };
+
+// This is virtually the same as using an array and indexing with enum values, \
+	but works consistently even when the enum literals are out of order. \
+	This decision is not consistent because the maps from controls to binds in \
+	EditData and GameData use arrays, which I probably have to fix, unless this\
+	option causes overhead (but i entrust the compiler that it won't)
+inline std::map<Controls, Control> gameControls{
+	{up, {KEY_E, "up"}},		{down, {KEY_D, "down"}},
+	{left, {KEY_S, "left"}},	{jump, {KEY_SPACE, "jump"}},
+	{look, {KEY_NULL,"look"}},	{toggleCamera, {KEY_TAB, "toggle camera"}},
+	{reset, {KEY_R, "reset"}},
+};
+
+inline std::map<EditControls, Control> editControls {
+	{scroll, {MOUSE_BUTTON_RIGHT, "scroll"}},	{select, {MOUSE_BUTTON_LEFT, "select"}},
+	{create, {KEY_F, "create"}},				{resize_w, {KEY_D, "horizontal resize"}},
+	{resize_h, {KEY_S, "vertical resize"}},		{move_up, {KEY_UP, "move up"}},
+	{move_down, {KEY_DOWN, "move down"}},		{move_left, {KEY_LEFT, "move left"}},
+	{move_right, {KEY_RIGHT, "move right"}},	{copy, {KEY_C, "copy"}},
+	{_delete, {KEY_DELETE, "delete"}},			{save, {KEY_K, "save"}},
+	{load, {KEY_L, "load"}},
+};
+
+const int controlsCount = 8;
+
+struct {
+	float minLength = 4;
+	const char* mapFileDir = "../resources/";
+	const char* controlsDisplay =
+		"Scroll:\tright mouse button\n"
+		"Select:\tleft mouse button\n"
+		"Create:\tF\n"
+		"X Resize:\tS\n"
+		"Y Resize:\tD\n"
+		"Move block:\tArrow keys\n"
+		"Clone:\tC\n"
+		"Delete:\tDel\n\n\n"
+		"Save map:\tK\n"
+		"Load map:\tL\n";
+} constData;
+
+struct EditData {
+	int controls[editControlsCount]{
+		MOUSE_BUTTON_RIGHT, MOUSE_BUTTON_LEFT, KEY_F,
+		KEY_S, KEY_D, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT,
+		KEY_C, KEY_DELETE,
+		KEY_K, KEY_L
+	};
+	std::vector<Rectangle>& structures;
+	Rectangle* selectedStructure = nullptr;
+	Camera2D camera{{0,0},{0,0},0.0f,1.0f};
+
+	EditData(std::vector<Rectangle>& _structures)
+	:	structures{_structures}{}
+};
+
+class Player {
+	int x, y;
+	int height, width;
+
+	void moveAxis(int moveAm, bool& axisCollide, Vector2 shift, Rectangles recs);
+public:
+	Texture tex;
+	bool collidingX=false, collidingY=false, onGround=false;
+	Player(int _x, int _y, int _width, int _height)
+	:	x{_x}, y{_y}
+	,	width{_width}, height{_height}{}
+
+	Player(int _x, int _y, int _width, int _height, Texture _tex)
+	:	x{_x}, y{_y}
+	,	width{_width}, height{_height}
+	,	tex{_tex}{}
+
+	Vector2 position(){ return {(float)x, (float)y}; }
+	Rectangle rectangle(){
+		return {(float)x, (float)y, (float)width, (float)height};
+	}
+
+	Player& moveX(float speed, Rectangles recs);
+	Player& moveY(float speed, Rectangles recs);
+	Player& move(Vector2 speed, Rectangles recs) {
+		return (*this)
+			.moveX(speed.x, recs)
+			.moveY(speed.y, recs);
+	}
+};
+
+struct GameData{
+	int controls[controlsCount]{KEY_E, KEY_D, KEY_S, KEY_F, KEY_SPACE,
+		MOUSE_BUTTON_LEFT, KEY_TAB, KEY_R};
+	Rectangle player{resolution.width/2.0f, resolution.height/2.0f, 60, 60};
+//	Rectangle structure{300,900,1800,30};
+	Camera2D camera;
+	std::vector<Rectangle> structures{};
+	Vector2 maxVel{454,40000};
+	Vector2 accel{1777,0};
+	Vector2 gravity{0,3000};
+	int cameraMode{mouselook};
+	int cameraModeCount = 2;
+	Player _player{(int)resolution.width/2, (int)resolution.height/2, 60, 60};
+	float jumpImpulse = 900;
+};
