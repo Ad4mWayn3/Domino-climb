16e2c2452a3f8e3fa8c33bc2dd0b34107bca391e
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-02-18 23:11:13 -0500

    including data.hpp

--- include/levelEditor.hpp
+++ include/levelEditor.hpp
@@ -1,51 +1,6 @@
 #pragma once
 
-#include <raylib.h>
-#include <raymath.h>
-
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <vector>
-
-enum EditControls {
-	scroll=0, select, create,
-	resize_w, resize_h, move_up, move_down, move_left, move_right,
-	copy, _delete,
-	save, load
-};
-
-struct {
-	float minLength = 4;
-	const char* mapFileDir = "../resources/";
-	const char* controlsDisplay =
-		"Scroll:\tright mouse button\n"
-		"Select:\tleft mouse button\n"
-		"Create:\tF\n"
-		"X Resize:\tS\n"
-		"Y Resize:\tD\n"
-		"Move block:\tArrow keys\n"
-		"Clone:\tC\n"
-		"Delete:\tDel\n\n\n"
-		"Save map:\tK\n"
-		"Load map:\tL\n";
-} constData;
-
-struct EditData {
-	int controls[13]{
-		MOUSE_BUTTON_RIGHT, MOUSE_BUTTON_LEFT, KEY_F,
-		KEY_S, KEY_D, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT,
-		KEY_C, KEY_DELETE,
-		KEY_K, KEY_L
-	};
-	std::vector<Rectangle>& structures;
-	Rectangle* selectedStructure = nullptr;
-	Camera2D camera{{0,0},{0,0},0.0f,1.0f};
-
-	EditData(std::vector<Rectangle>& _structures)
-	:	structures{_structures}{}
-};
+#include "data.hpp"
 
 void editProcess(float delta, EditData& editData);
 
--- include/platformer.hpp
+++ include/platformer.hpp
@@ -1,90 +1,9 @@
 #pragma once
 
-#include <raylib.h>
-#include <raymath.h>
- 
-#include <iostream>
-#include <sstream>
-#include <vector>
-
-#define MIN_BOUNCE_SPEED 100
-
-using Rectangles = const std::vector<Rectangle>&;
-
-const struct {
-	int width, height;
-} resolution{1920, 1080};
-
-const Vector2 resolutionV{resolution.width, resolution.height};
+#include "data.hpp"
 
 bool checkCollisions(Rectangle rec, Rectangles recs);
 
-enum Axis {horizontal, vertical};
-
 bool checkAdjacents(Rectangle rec, Rectangles recs, Axis axis, bool& onGround);
-
-class Player {
-	int x, y;
-	int height, width;
-
-	void moveAxis(int moveAm, bool& axisCollide, Vector2 shift, Rectangles recs) {
-		while (moveAm) {
-			if (!checkCollisions({float(x+shift.x), float(y+shift.y), \
-				(float)width, (float)height}, recs)) {
-				axisCollide = true;
-				return;
-			}
-			x += shift.x;
-			y += shift.y;
-			moveAm -= shift.x + shift.y;
-		}
-		axisCollide = false;
-	}
-public:
-	Texture tex;
-	bool collidingX=false, collidingY=false, onGround=false;
-	Player(int _x, int _y, int _width, int _height)
-	:	x{_x}, y{_y}
-	,	width{_width}, height{_height}{}
-
-	Player(int _x, int _y, int _width, int _height, Texture _tex)
-	:	x{_x}, y{_y}
-	,	width{_width}, height{_height}
-	,	tex{_tex}{}
-
-	Vector2 position(){ return {(float)x, (float)y}; }
-	Rectangle rectangle(){
-		return {(float)x, (float)y, (float)width, (float)height};
-	}
-
-	Player& moveX(float speed, Rectangles recs);
-	Player& moveY(float speed, Rectangles recs);
-	Player& move(Vector2 speed, Rectangles recs) {
-		return (*this)
-			.moveX(speed.x, recs)
-			.moveY(speed.y, recs);
-	}
-};
-enum Controls{up=0, down, left, right, jump, look, toggleCamera, reset};
-enum CamMode{mouselook=0, follow};
-
-struct GameData{
-	int controls[8]{KEY_E, KEY_D, KEY_S, KEY_F, KEY_SPACE,
-		MOUSE_BUTTON_LEFT, KEY_TAB, KEY_R};
-	Rectangle player{resolution.width/2.0f, resolution.height/2.0f, 60, 60};
-//	Rectangle structure{300,900,1800,30};
-	Camera2D camera;
-	std::vector<Rectangle> structures{};
-	Vector2 maxVel{454,40000};
-	Vector2 accel{1777,0};
-	Vector2 gravity{0,3000};
-	int cameraMode{mouselook};
-	int cameraModeCount = 2;
-	Player _player{resolution.width/2.0f, resolution.height/2.0f, 60, 60};
-	float jumpImpulse = 900;
-};
-
-
-void _process(float delta, GameData& gameData);
 
 void process(float delta, GameData& gameData);
